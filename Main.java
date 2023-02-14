d9f1da926ac3a6255ca09a0cbb3dfc8a8ca88624
MAHRI ANTONGIOVANNI <123492202+mahriAnton@users.noreply.github.com>
2023-02-14 17:19:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main{
+    public static void main (String[] args){
+    
+        
+        //create a frame
+        //JFrame myFrame = new JFrame();
+        //myFrame.setSize(400,400);
+        
+        //create a panel
+      
+        //BallPanel myBall = new BallPanel();
+        
+        //add stuff to panel
+      
+        
+        //and panel to frame
+       
+       // myFrame.add(myBall);
+        
+        //make frame visible
+       // myFrame.setVisible(true);
+        
+        
+        
+    }
+}
